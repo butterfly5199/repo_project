3dd2b0f553fa0ea2ac9fa5082b2fc37d75eb7ad5
butterfly5199 <snr5199@gmail.com>
2025-02-28 12:31:20 +0900

    25-02-28-1231

--- project/src/main/java/com/a5a5lab/fapp/group1/Group1Controller.java
+++ project/src/main/java/com/a5a5lab/fapp/group1/Group1Controller.java
@@ -1,7 +1,11 @@
 package com.a5a5lab.fapp.group1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,12 +15,24 @@ public class Group1Controller {
 // html로 데이터 전달
 	
 	@Autowired
-	
-	
-	
+	Group1Service group1Service;
+			
 	@RequestMapping(value = "/group1/group1XdmList")
-	public String group1XdmList() {
+	public String group1XdmList(Model model) {
+		List<Group1Dto> group1Dtos = new ArrayList<>();
+		
+		group1Dtos = group1Service.selectList();	
+		
+		model.addAttribute("list",group1Dtos);		
+		
 		return "group1/group1XdmList";
 	}
+	
+//	@RequestMapping(value = "/group1/group1XdmList")
+//	public String group1XdmList(Model model) {
+//		model.addAttribute("list",group1Service.group1XdmList);		
+//		
+//		return "group1XdmList";
+//	}
 
 }
