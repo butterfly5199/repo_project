59ff3c550f2fe805d0c1c87f032370fe12c51f86
butterfly5199 <snr5199@gmail.com>
2025-03-06 14:10:44 +0900

    25-03-06.1410

--- project/src/main/java/com/a5a5lab/fapp/contact/ContactController.java
+++ project/src/main/java/com/a5a5lab/fapp/contact/ContactController.java
@@ -29,7 +29,36 @@ public class ContactController {
 		model.addAttribute("list",contactService.selectList());		
 		return "contact/contactXdmList";
 	}
-		
 	
+	@RequestMapping(value="/contact/contactXdmView")
+	public String contactXdmView(Model model, ContactDto contactDto) {
+		
+		model.addAttribute("item", contactService.selectOne(contactDto));
+		return "contact/contcatXdmView";
+	}
+	
+	@RequestMapping(value="/contact/contactXdmForm")
+	public String contactXdmForm() {
+		
+		return "contact/contactXdmForm";
+	}
+	
+	@RequestMapping(value="/contact/contactXdmInst")
+	public String contactXdmInst(ContactDto contactDto) {
+		System.out.println("contactDto.getSeq(): " + contactDto.getSeq());
+		System.out.println("contactDto.getName(): " + contactDto.getName());
+		
+		contactSevice.insert(contactDto);
+		System.out.println("contactDto.getSeq(): " + contactDto.getSeq());
+		
+		return  "rledirect:/contact/contactXdmList";
+		
+	}
+		
+	@RequestMapping(value="/contact/contactXdmMfom")
+	public String contactXdmMfom(ContactDto contactDto, Model model) {
+		
+		model.addAtt
+	}
 	
 }
